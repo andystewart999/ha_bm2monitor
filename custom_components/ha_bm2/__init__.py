2e3c3f62fb7a36197a1108207b0bc7d12b3f63b3
Andy Stewart <andy.stewart@live.com>
2024-11-02 15:18:05 +1100

    Update __init__.py

--- custom_components/ha_bm2/__init__.py
+++ custom_components/ha_bm2/__init__.py
@@ -1,82 +1,48 @@
-"""Config flow for victron_ble integration."""
+"""The victron_ble integration."""
 from __future__ import annotations
 
 import logging
-from typing import Any
 
-import voluptuous as vol
-from homeassistant import config_entries
-from homeassistant.components.bluetooth import BluetoothServiceInfoBleak
-from homeassistant.data_entry_flow import FlowResult
-from homeassistant.exceptions import HomeAssistantError
+from homeassistant.components.bluetooth import BluetoothScanningMode
+from homeassistant.components.bluetooth.passive_update_processor import (
+    PassiveBluetoothProcessorCoordinator,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .device import VictronBluetoothDeviceData
+
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required("Title"): str,
-        vol.Required("key"): str,
-    }
-)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Victron BLE device from a config entry."""
+    address = entry.unique_id
+    assert address is not None
+    data = VictronBluetoothDeviceData(entry.data["key"])
+    coordinator = hass.data.setdefault(DOMAIN, {})[
+        entry.entry_id
+    ] = PassiveBluetoothProcessorCoordinator(
+        hass,
+        _LOGGER,
+        address=address,
+        mode=BluetoothScanningMode.ACTIVE,
+        update_method=data.update,
+    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(
+        coordinator.async_start()
+    )  # only start after all platforms have had a chance to subscribe
+    return True
 
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # type: ignore[call-arg]
-    """Handle a config flow for victron_ble."""
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
 
-    VERSION = 1
-
-    async def async_step_bluetooth(
-        self, discovery_info: BluetoothServiceInfoBleak
-    ) -> FlowResult:
-        """Handle a flow initialized by bluetooth discovery."""
-        _LOGGER.debug(discovery_info)
-        self.context["discovery_info"] = {
-            "name": discovery_info.name,
-            "address": discovery_info.address,
-        }
-        await self.async_set_unique_id(discovery_info.address)
-        self._abort_if_unique_id_configured()
-        return await self.async_step_user()
-
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """User setup."""
-        if user_input is None:
-            name = None
-            address = None
-
-            discovery_info = self.context.get("discovery_info")
-            if discovery_info:
-                name = self.context["discovery_info"]["name"]
-                address = self.context["discovery_info"]["address"]
-
-            return self.async_show_form(
-                step_id="user",
-                data_schema=vol.Schema(
-                    {
-                        vol.Required("name", default=name): str,
-                        vol.Required("address", default=address): str,
-                        vol.Required("key"): str,
-                    }
-                ),
-            )
-
-        await self.async_set_unique_id(user_input["address"])
-        self._abort_if_unique_id_configured()
-        return self.async_create_entry(title=user_input["name"], data=user_input)
-
-    async def async_step_unignore(self, user_input):
-        unique_id = user_input["unique_id"]
-        await self.async_set_unique_id(unique_id)
-        self.async_abort(reason="discovery_error")
-
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(HomeAssistantError):
-    """Error to indicate there is invalid auth."""
+    return unload_ok
