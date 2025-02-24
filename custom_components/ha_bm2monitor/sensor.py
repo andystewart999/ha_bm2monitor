26da035825fd285c57343ac82e95ddc42e3df05a
Andy Stewart <andy.stewart@live.com>
2025-02-24 15:19:26 +1100

    Update sensor.py

--- custom_components/ha_bm2monitor/sensor.py
+++ custom_components/ha_bm2monitor/sensor.py
@@ -1,136 +1,127 @@
-"""Interfaces with the Integration 101 Template api sensors."""
+"""Support for BMx battery monitor sensors."""
 
-import logging
+from __future__ import annotations
+
+from .bmx_ble import BMxSensor, SensorUpdate
+
+from homeassistant.components.bluetooth.passive_update_processor import (
+    PassiveBluetoothDataProcessor,
+    PassiveBluetoothDataUpdate,
+    PassiveBluetoothProcessorEntity,
+    PassiveBluetoothEntityKey
+)
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
+    SensorEntityDescription,
     SensorStateClass,
 )
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     PERCENTAGE,
-    UnitOfElectricPotential,
+    SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+    EntityCategory,
+    UnitOfTime,
+    UnitOfElectricPotential
 )
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.components.sensor import SensorDeviceClass
+from homeassistant.helpers.sensor import sensor_device_info_to_hass_device_info
 
-from .const import (
-    DOMAIN,
-    Device,
-    DeviceType
-)
+from . import BMxConfigEntry
+from .device import device_key_to_bluetooth_entity_key
+from .const import BATTERY_STATUS_ICON
 
-from .coordinator import ExampleCoordinator
-from bluetooth_data_tools import human_readable_name
+import logging
+_LOGGER = logging.getLogger(__name__)
 
-#_LOGGER = logging.getLogger(__name__)
+SENSOR_DESCRIPTIONS: dict[str, SensorEntityDescription] = {
+    BMxSensor.BATTERY_VOLTAGE: SensorEntityDescription(
+        key=BMxSensor.BATTERY_VOLTAGE,
+        device_class=SensorDeviceClass.VOLTAGE,
+        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
+        name="Voltage",
+        icon="mdi:current-dc"
+    ),
+    BMxSensor.BATTERY_STATUS: SensorEntityDescription(
+        key=BMxSensor.BATTERY_STATUS,
+        translation_key="status",
+        name="Status"
+    ),
+    BMxSensor.BATTERY_PERCENT: SensorEntityDescription(
+        key=BMxSensor.BATTERY_PERCENT,
+        device_class=SensorDeviceClass.BATTERY,
+        native_unit_of_measurement=PERCENTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+        name="Percent"
+    ),
+    BMxSensor.SIGNAL_STRENGTH: SensorEntityDescription(
+        key=BMxSensor.SIGNAL_STRENGTH,
+        device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+        native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+        state_class=SensorStateClass.MEASUREMENT,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        entity_registry_enabled_default=False,
+        name="Signal strength"
+    ),
+}
+
+
+def sensor_update_to_bluetooth_data_update(
+    sensor_update: SensorUpdate,
+    ) -> PassiveBluetoothDataUpdate:
+
+    """Convert a sensor update to a bluetooth data update."""
+    return PassiveBluetoothDataUpdate(
+        devices={
+            device_id: sensor_device_info_to_hass_device_info(device_info)
+            for device_id, device_info in sensor_update.devices.items()
+        },
+        entity_descriptions={
+            device_key_to_bluetooth_entity_key(device_key): SENSOR_DESCRIPTIONS[
+                device_key.key
+            ]
+            for device_key in sensor_update.entity_descriptions
+        },
+        entity_data={
+            device_key_to_bluetooth_entity_key(device_key): sensor_values.native_value
+            for device_key, sensor_values in sensor_update.entity_values.items()
+        },
+        entity_names={},
+    )
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigEntry,
+    entry: BMxConfigEntry,
     async_add_entities: AddEntitiesCallback,
-):
-    """Set up the Sensors."""
-    # This gets the data update coordinator from hass.data as specified in your __init__.py
-    coordinator: ExampleCoordinator = hass.data[DOMAIN][
-        config_entry.entry_id
-    ].coordinator
+    ) -> None:
 
-    # Enumerate all the sensors in your data value from your DataUpdateCoordinator and add an instance of your sensor class
-    # to a list for each one.
-    sensors = [
-        ExampleSensor(coordinator, device)
-        for device in coordinator.data.devices
-    ]
-
-    # Create the sensors.
-    async_add_entities(sensors, update_before_add = True)
-
-class ExampleSensor(CoordinatorEntity, SensorEntity):
-    """Implementation of a sensor."""
-
-    def __init__(self, coordinator: ExampleCoordinator, device: Device) -> None:
-        """Initialise sensor."""
-        super().__init__(coordinator)
-        self.device = device
-        self.device_unique_id = device.device_unique_id
-
-
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Update sensor with latest data from coordinator."""
-        # This method is called by your DataUpdateCoordinator when a successful update runs.
-        self.device = self.coordinator.get_device_by_type(
-            self.device.device_type)
-            
-        if not self.device is None:
-            self.async_write_ha_state()
-
-    @property
-    def device_class(self) -> SensorDeviceClass | None:
-        """Return device class."""
-        # https://developers.home-assistant.io/docs/core/entity/sensor/#available-device-classes
-        return self.device.device_class
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return device information."""
-        # Identifiers are what group entities into the same device.
-        # If your device is created elsewhere, you can just specify the indentifiers parameter.
-        # If your device connects via another device, add via_device parameter with the indentifiers of that device.
-        return DeviceInfo(
-            name=f"{human_readable_name(None, "BM2 battery monitor", self.coordinator.address)}",
-            manufacturer="Shenzhen Leagend Optoelectronics",
-            model="BM2",
-            model_id=self.coordinator.address,
-            identifiers={
-                (
-                    DOMAIN,
-                    self.coordinator.address,
-                )
-            },
+    """Set up the BMx BLE sensors."""
+    coordinator = entry.runtime_data
+    processor = PassiveBluetoothDataProcessor(sensor_update_to_bluetooth_data_update)
+    entry.async_on_unload(
+        processor.async_add_entities_listener(
+            BMxBluetoothSensorEntity, async_add_entities
         )
+    )
+    entry.async_on_unload(
+        coordinator.async_register_processor(processor, SensorEntityDescription)
+    )
+
+
+class BMxBluetoothSensorEntity(
+    PassiveBluetoothProcessorEntity[
+        PassiveBluetoothDataProcessor[str | int | None, SensorUpdate]
+    ],
+    SensorEntity,
+    ):
+    """Representation of a BMX BLE sensor."""
 
     @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return self.device.name
-
-    @property
-    def native_value(self) -> int | float | str:
-        """Return the state of the entity."""
-        # Using native value and native unit of measurement, allows you to change units
-        # in Lovelace and HA will automatically calculate the correct value.
-        return self.device.state
-
-    @property
-    def native_unit_of_measurement(self) -> UnitOfElectricPotential | None:
-        """Return unit of temperature."""
-        return self.device.device_unit
-
-    @property
-    def unique_id(self) -> str:
-        """Return unique id."""
-        # All entities must have a unique id.  Think carefully what you want this to be as
-        # changing it later will cause HA to create new entities.
-        return self.device.device_unique_id
-
-    @property
-    def extra_state_attributes(self):
-        """Return the extra state attributes."""
-        # Add any additional attributes you want on your sensor.
-        attrs = {}
-        attrs["battery_type"] = self.coordinator.battery_type
-        return attrs
-
-    @property
-    def icon(self) -> str:
-        """Return icon, only for the status entity."""
-        return self.device.device_icon
+    def native_value(self) -> str | int | None:
+        """Return the native value."""
+        return self.processor.entity_data.get(self.entity_key)
 
     @property
     def available(self) -> bool:
@@ -144,7 +135,24 @@ class ExampleSensor(CoordinatorEntity, SensorEntity):
         """
         return True
 
-    # @property
-    # def assumed_state(self) -> bool:
-    #     """Return True if the device is no longer broadcasting."""
-    #     return not self.processor.available
+    @property
+    def assumed_state(self) -> bool:
+        """Return True if the device is no longer broadcasting."""
+        return not self.processor.available
+
+#   @property
+#    def extra_state_attributes(self):
+#        """Return the extra state attributes."""
+#        # Add any additional attributes you want on your sensor.
+#        attrs = {}
+#        attrs["battery_type"] = self.coordinator.battery_type
+#        return attrs
+#
+    @property
+    def icon(self) -> str:
+
+        """Return icon only for the status and volts entities."""
+        if self.entity_key.key == "battery_status":
+            return BATTERY_STATUS_ICON.get(self.processor.entity_data.get(self.entity_key), "mdi:battery-off")
+        elif self.entity_key.key == "battery_voltage":
+            return "mdi:current-dc"
