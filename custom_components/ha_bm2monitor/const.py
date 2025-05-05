0abdd0f3b2a3d2edf098f106ac891c14bfc53cfd
Andy Stewart <andy.stewart@live.com>
2025-05-06 08:08:07 +1000

    Update const.py
    Added new 'custom battery' option

--- custom_components/ha_bm2monitor/const.py
+++ custom_components/ha_bm2monitor/const.py
@@ -9,6 +9,25 @@ MIN_SCAN_INTERVAL = 30
 CONF_SCAN_MODE = "scan_mode"
 DEFAULT_SCAN_MODE = "Always rate limit sensor updates"
 
+CONF_CUSTOM_BATTERY_CHEMISTRY = "custom_battery_chemistry"
+DEFAULT_CUSTOM_BATTERY_CHEMISTRY = "Custom battery"
+CONF_CUSTOM_CRITICAL_VOLTAGE = "custom_critical_voltage"
+DEFAULT_CUSTOM_CRITICAL_VOLTAGE = 12.06
+CONF_CUSTOM_LOW_VOLTAGE = "custom_low_voltage"
+DEFAULT_CUSTOM_LOW_VOLTAGE = 12.2
+CONF_CUSTOM_FIFTY_PERCENT_VOLTAGE = "custom_fifty_percent_voltage"
+DEFAULT_CUSTOM_FIFTY_PERCENT_VOLTAGE = 12.3
+CONF_CUSTOM_HUNDRED_PERCENT_VOLTAGE = "custom_hundred_percent_voltage"
+DEFAULT_CUSTOM_HUNDRED_PERCENT_VOLTAGE = 12.7
+CONF_CUSTOM_FLOATING_VOLTAGE = "custom_floating_voltage"
+DEFAULT_CUSTOM_FLOATING_VOLTAGE = 13.7
+CONF_CUSTOM_CHARGING_VOLTAGE = "custom_charging_voltage"
+DEFAULT_CUSTOM_CHARGING_VOLTAGE = 14.5
+CONF_CUSTOM_NUMPY_VOLTS = "custom_numpy_volts"
+DEFAULT_CUSTOM_NUMPY_VOLTS = [10.5, 11.58, 12.06, 13.6]
+CONF_CUSTOM_NUMPY_PERCENT = "custom_numpy_percent"
+DEFAULT_CUSTOM_NUMPY_PERCENT = [0, 20, 50, 100]
+
 GATT_TIMEOUT = 20
 
 BM_NAMES = [
@@ -31,7 +50,9 @@ BATTERY_TYPES = [
     "Deep-cycle",
     "Lead-acid",
     "LiFePO4",
-    "Lithium-ion"
+    "iTechworld 120X (LiFePO4)",
+    "Lithium-ion",
+    "Custom"
 ]
 
 BATTERY_STATUS_LIST = {
